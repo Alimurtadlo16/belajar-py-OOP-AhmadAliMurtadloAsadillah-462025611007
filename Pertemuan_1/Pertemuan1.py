56358c7c4feae2c2a5e9649df2d054c5fce7f5df
Alimurtadlo16 <ahmadalimurtadloasadillah@gmail.com>
2026-05-11 16:38:40 +0700

    Materi 1: Dasar Class dan Object

---
+++ Pertemuan_1/Pertemuan1.py
@@ -0,0 +1,23 @@
+# Materi Pertemuan 1.
+class Kampus:
+    pass
+
+class Mahasiswa:
+    nama = ""
+    nim = ""
+
+print(Kampus)
+print(Mahasiswa)
+
+mahasiswa1 = Mahasiswa()
+mahasiswa1.nama = "Ali"
+mahasiswa1.nim = "123456789"
+
+mahasiswa2 = Mahasiswa()
+mahasiswa2.nama = "Budi"
+mahasiswa2.nim = "987654321"
+
+print(mahasiswa1.nama)
+print(mahasiswa1.nim)
+print(mahasiswa2.nama)
+print(mahasiswa2.nim)
